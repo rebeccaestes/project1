4a906844869f785fac66793dd90725fef5aed631
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-10-26 10:26:37 -0400

    display all cards with icons

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ var swords = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "
 var arrows = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
 var pistols = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
 var cannons = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
-var special = ["jack1", "jack2"];
+var jokers = ["joker1", "joker2"];
 
-var deck = [swords, arrows, pistols, cannons, special];
+var deck = [swords, arrows, pistols, cannons, jokers];
 
-// for (i = 0, i < deck.length, i++) {
-// 	for (j = 0, j < swords.length, j++) {
-		
-// 	}
-// }
+for (var i = 0; i < swords.length; i++) {
+	$("#swords").append("<div class='card'><p>" + swords[i] + "</p><img src='img/sword.png' alt='sword'></div>");
+}
+for (var i = 0; i < arrows.length; i++) {
+	$("#arrows").append("<div class='card'><p>" + arrows[i] + "</p><img src='img/arrow.png' alt='sword'></div>");
+}
+for (var i = 0; i < pistols.length; i++) {
+	$("#pistols").append("<div class='card'><p>" + pistols[i] + "</p><img src='img/pistol.png' alt='sword'></div>");
+}
+for (var i = 0; i < cannons.length; i++) {
+	$("#cannons").append("<div class='card'><p>" + cannons[i] + "</p><img src='img/cannon.png' alt='sword'></div>");
+}
+for (var i = 0; i < jokers.length; i++) {
+	$("#jokers").append("<div class='card'><p>" + jokers[i] + "</p><img src='img/joker.png' alt='sword'></div>");
+}
 
-// var cardVal;
+// $(".awesome").append("<div>this div is appended</div>")
+
+
+// Math.floor((Math.random() * 53)
