814a938be47a0299cd75ad6fffa0ff31b618a9be
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-10-26 09:37:15 -0400

    arrays of cards

---
+++ script.js
@@ -0,0 +1,15 @@
+var swords = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+var arrows = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+var pistols = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+var cannons = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+var special = ["jack1", "jack2"];
+
+var deck = [swords, arrows, pistols, cannons, special];
+
+// for (i = 0, i < deck.length, i++) {
+// 	for (j = 0, j < swords.length, j++) {
+		
+// 	}
+// }
+
+// var cardVal;
